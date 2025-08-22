a3581ace643ae0ad7c07e535c08829c2de044cda
ashutosh <radaashutosh556@gmail.com>
2025-08-22 22:55:33 +0530

    "

--- packages/main/src/components/shadcn-ui/Button/ButtonWithIcon.tsx
+++ packages/main/src/components/shadcn-ui/Button/ButtonWithIcon.tsx
@@ -1,39 +1,20 @@
-
-
-import CardBox from '../../shared/CardBox'
-import { Button } from 'src/components/shadcn-ui/Default-Ui/button'
-import { Icon } from '@iconify/react/dist/iconify.js'
+import CardBox from '../../shared/CardBox';
+import CodeDialog from '../../shared/CodeDialog';
+import ButtonWithicon from './code/ButtonWithIconCode';
+import ButtonWithiconCode from './code/ButtonWithIconCode.tsx?raw';
 
 const ButtonWithIcon = () => {
   return (
-    <CardBox>
-      <div className='flex items-center justify-between'>
-        <h4 className='text-lg font-semibold'> Button With Icon</h4>
-      </div>
-      <div className='flex flex-wrap items-center gap-3 mt-4'>
-        <Button>
-          {' '}
-          <Icon icon='tabler:mail' className='text-lg' /> Login with email
-        </Button>
-        <Button variant='secondary'>
-          <Icon icon='tabler:garden-cart' className='text-lg' /> Shop now
-        </Button>
-        <Button variant='success'>
-          <Icon icon='tabler:user-circle' className='text-lg' /> Signup now
-        </Button>
-        <Button variant='warning'>
-          <Icon icon='tabler:calendar-event' className='text-lg' /> Live event
-        </Button>
-        <Button variant='info'>
-          <Icon icon='tabler:align-box-left-bottom' className='text-lg' />
-          Create project
-        </Button>
-        <Button variant='error'>
-          <Icon icon='tabler:user-shield' className='text-lg' /> Login now
-        </Button>
+    <CardBox className="p-0">
+      <div>
+        <div className="p-6">
+          <h4 className="text-lg font-semibold mb-4"> Button With Icon</h4>
+          <ButtonWithicon />
+        </div>
+        <CodeDialog>{ButtonWithiconCode}</CodeDialog>
       </div>
     </CardBox>
-  )
-}
+  );
+};
 
-export default ButtonWithIcon
+export default ButtonWithIcon;
